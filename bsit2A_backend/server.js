292e6a358321e2ee651eda96b9fe56f1d3e1cd85
Arvin John Posadas <ajposadas116@gmail.com>
2025-10-22 08:48:27 +0800

    ARBIN JAY

---
+++ bsit2A_backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const studentRoutes = require('./routes/studentRoute');
+const departmentRoutes = require('./routes/departmentRoute');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api/students', studentRoutes);
+app.use('/api/departments', departmentRoutes)
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+//   console.log(`API URL: http://localhost:${PORT}/api/students`);
+});
